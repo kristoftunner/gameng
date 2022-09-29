5b2f0c38103588d70be081f5a0e15e5da194a5e7
kitu <kristoftunner95@gmail.com>
2022-09-29 21:26:12 +0200

    finished Vertex and IndexBuffer

--- src/gameng/application.cpp
+++ src/gameng/application.cpp
@@ -12,6 +12,27 @@ namespace gameng
 
 Application* Application::s_instance = nullptr;
 
+static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+{
+  switch(type)
+  {
+    case ShaderDataType::Float:   return GL_FLOAT; 
+    case ShaderDataType::Float2:return GL_FLOAT; 
+    case ShaderDataType::Float3:return GL_FLOAT; 
+    case ShaderDataType::Float4:return GL_FLOAT; 
+    case ShaderDataType::Int:     return GL_INT; 
+    case ShaderDataType::Int2:return GL_INT; 
+    case ShaderDataType::Int3:return GL_INT; 
+    case ShaderDataType::Int4:return GL_INT; 
+    case ShaderDataType::Mat3:return GL_FLOAT; 
+    case ShaderDataType::Mat4:return GL_FLOAT; 
+    case ShaderDataType::Bool:return GL_BOOL; 
+  }
+
+  GAMENG_CORE_ERR("Unknown ShaderDataType");
+  return 0;
+}
+
 Application::Application()
 {
   if(s_instance == nullptr)
@@ -30,16 +51,30 @@ Application::Application()
   glGenVertexArrays(1, &m_vertexArray);
   glBindVertexArray(m_vertexArray);
 
-  float vertices[3*3] = {
-    -0.5f, -0.5f, 0.0f,
-    0.5f, -0.5f, 0.0f,
-    0.0f, 0.5f, 0.0f
+  float vertices[3*7] = {
+    -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+    0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 
+    0.0f, 0.5f, 0.0f,1.0f, 0.0f, 0.0f, 1.0f 
   };
 
   m_vertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
   
-  glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), nullptr);
+  BufferLayout layout = {
+    {ShaderDataType::Float3, "a_position"},
+    {ShaderDataType::Float4, "a_color"}
+  };
+  
+  m_vertexBuffer->SetLayout(layout);
+
+  uint32_t index = 0;
+  for(const auto& element : m_vertexBuffer->GetLayout())
+  {
+    glEnableVertexAttribArray(index);
+    glVertexAttribPointer(index, element.GetComponentCount(), ShaderDataTypeToOpenGLBaseType(element.type), element.normalized ? GL_TRUE : GL_FALSE, layout.GetStride(), (const void*)element.offset);
+    index++;
+  }
+
+
 
   unsigned int indices[3] = {0,1,2};
   m_indexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
@@ -48,12 +83,15 @@ Application::Application()
     #version 330 core
     
     layout(location=0) in vec3 a_position;
+    layout(location=1) in vec4 a_color;
 
     out vec3 v_position;
+    out vec4 v_color;
 
     void main()
     {
       v_position = a_position;
+      v_color = a_color;
       gl_Position = vec4(a_position, 1.0);
     }
   )";
@@ -64,10 +102,12 @@ Application::Application()
     layout(location=0) out vec4 color;
 
     in vec3 v_position;
+    in vec4 v_color;
 
     void main()
     {
       color = vec4(v_position * 0.5 + 0.5, 1.0);
+      color = v_color;
     }
   )";
   m_shader.reset(new Shader(vertexSrc, fragmentSource));
--- src/gameng/renderer/buffer.hpp
+++ src/gameng/renderer/buffer.hpp
@@ -1,9 +1,104 @@
 #pragma once
 
+#include "gameng/log.hpp"
+
 #include <stdint.h>
 
 namespace gameng
 {
+
+enum class ShaderDataType 
+{
+  None = 0, Float, Float2, Float3, Float4, Mat3, Mat4, Int, Int2, Int3, Int4, Bool
+};
+static uint32_t ShaderDataTypeSize(ShaderDataType type)
+{
+  switch(type)
+  {
+    case ShaderDataType::Float:  return sizeof(float);
+    case ShaderDataType::Float2: return sizeof(float)*2;
+    case ShaderDataType::Float3: return sizeof(float)*3;
+    case ShaderDataType::Float4: return sizeof(float)*4;
+    case ShaderDataType::Mat3:   return sizeof(float)*3*3;
+    case ShaderDataType::Mat4:   return sizeof(float)*4*4;
+    case ShaderDataType::Int:    return sizeof(int);
+    case ShaderDataType::Int2:   return sizeof(int)*2;
+    case ShaderDataType::Int3:   return sizeof(int)*3;
+    case ShaderDataType::Int4:   return sizeof(int)*4;
+    case ShaderDataType::Bool:   return 1;
+    default: GAMENG_CORE_ERR("Unknown shader data type");
+  }
+
+  return 0;
+}
+
+struct BufferElement
+{
+  std::string name;
+  ShaderDataType type;
+  uint32_t offset;
+  uint32_t size;
+  uint32_t count;
+  bool normalized;
+
+  BufferElement(){}
+  BufferElement( ShaderDataType type, const std::string& name, bool normalized = false)
+  : name(name), type(type), size(ShaderDataTypeSize(type)), offset(0), normalized(normalized) 
+  {
+
+  }
+  
+  uint32_t GetComponentCount() const
+  {
+    switch(type)
+    {
+      case ShaderDataType::Float:   return 1;
+      case ShaderDataType::Float2:  return 2;
+      case ShaderDataType::Float3:  return 3;
+      case ShaderDataType::Float4:  return 4;
+      case ShaderDataType::Int:   return 1;
+      case ShaderDataType::Int2:  return 2;
+      case ShaderDataType::Int3:  return 3;
+      case ShaderDataType::Int4:  return 4;
+      case ShaderDataType::Bool:  return 1;
+      case ShaderDataType::Mat3:  return 3*3;
+      case ShaderDataType::Mat4:  return 4*4;
+    }
+    GAMENG_CORE_ERR("Wrong element count");
+    return 0;
+  }
+};
+
+class BufferLayout
+{
+public:
+  BufferLayout(){}
+  BufferLayout(const std::initializer_list<BufferElement>& elements) : m_elements(elements) 
+  {
+    CalculateOffsetAndStride();
+  }
+  inline const std::vector<BufferElement>& GetElements() const {return m_elements;}
+  std::vector<BufferElement>::iterator begin() {return m_elements.begin();}
+  std::vector<BufferElement>::iterator end() {return m_elements.end();}
+  std::vector<BufferElement>::const_iterator begin() const {return m_elements.begin();}
+  std::vector<BufferElement>::const_iterator end() const {return m_elements.end();}
+  inline const uint32_t GetStride() const {return m_stride;}
+private:
+  void CalculateOffsetAndStride()
+  {
+    uint32_t offset = 0;
+    m_stride = 0;
+    for(auto& element : m_elements)
+    {
+      element.offset = offset;
+      offset += element.size;
+      m_stride += element.size;
+    }
+  }
+private:
+  std::vector<BufferElement> m_elements;
+  uint32_t m_stride = 0;
+};
 
 class VertexBuffer
 {
@@ -12,7 +107,8 @@ public:
   
   virtual void Bind() const {}
   virtual void UnBind() const {};
-
+  virtual void SetLayout(const BufferLayout& layout) = 0;
+  virtual const BufferLayout& GetLayout() const = 0;
   static VertexBuffer* Create(float* vertices, uint32_t size);
 };
 
